cafe8d982fce8d24547f80c88afb5cbc590e1628
Ubuntu <chemacarceller@yahoo.es>
2025-06-30 19:45:49 +0200

    gdscript changes quit request

--- demo/addons/rndmc/random_movement_nod.gd
+++ demo/addons/rndmc/random_movement_nod.gd
@@ -1,5 +1,10 @@
 class_name RandomMovementComponent extends Node
 
+# Property indicating if the movement is applied to an Actor or a Component
+# If the movement is applied to an Actor the local posiiton changes everyframe because is the global position
+# If the movement is applied to a Component the local position doesn't change
+@export var _isActorAttached : bool = true
+
 # Property to activate or deactivate the movement
 @export var _isEnabled : bool = true
 
@@ -18,28 +23,35 @@ func get_IsEnebled() -> bool :
 @onready var _parentActor : Node3D = get_parent()
 
 # Getting the ParentActor position
-@onready var _parentActorPosition : Vector3 = _parentActor.global_position
+@onready var _parentActorPosition : Vector3 = _parentActor.position
 
 # Flag indicating if the Offset should be calculated
 # The idea is the ParentActor moves and comes back to its position in the next frame (odd versus even frames)
 var _newOffset : bool = true
-var _offset : Vector3 = Vector3.ZERO
+
+func _notification(what):
+	if what == NOTIFICATION_WM_CLOSE_REQUEST:
+		_parentActor.queue_free()
+
 
 # the movement code
 func _physics_process(_delta: float) -> void:
 	
 	# Only if it is enabled
 	if _isEnabled :
+		# Offset to move
+		var offset : Vector3 = Vector3.ZERO
 
-		_parentActorPosition = _parentActor.global_position
+		if (_isActorAttached):
+			_parentActorPosition = _parentActor.position
 
 		# If we should calculate the offset to move
 		if (_newOffset) :
 			var rng = RandomNumberGenerator.new()
-			_offset = Vector3(rng.randf_range(-1.0, 1.0) * movementRadius, rng.randf_range(-1.0, 1.0) * movementRadius,rng.randf_range(-1.0, 1.0) * movementRadius)
-			_parentActor.global_position = _parentActorPosition + _offset
+			offset = Vector3(rng.randf_range(-1.0, 1.0) * movementRadius, rng.randf_range(-1.0, 1.0) * movementRadius,rng.randf_range(-1.0, 1.0) * movementRadius)
+			_parentActor.position = _parentActorPosition + offset
 		else:
-			_parentActor.global_position = _parentActorPosition - _offset
+			_parentActor.position = _parentActorPosition - offset
 
 		# Toogles the offset
 		_newOffset = not _newOffset
